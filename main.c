dcddb47b4f026f76a33a44a8df593f7a771d0f99
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-11-20 18:17:07 -0700

    LED PWM implemented to drive one wheel

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
-#include <msp430.h> 
+#include <msp430.h>
 
-/*
- * main.c
- */
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
-	return 0;
+void main(void)
+{
+    WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
+
+    P1DIR |= BIT2;                // TA0CCR1 on P1.2
+    P1SEL |= BIT2;                // TA0CCR1 on P1.2
+
+    TACTL &= ~MC1|MC0;            // stop timer A0
+
+    TACTL |= TACLR;                // clear timer A0
+
+    TACTL |= TASSEL1;           // configure for SMCLK
+
+    TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+    TACCR1 = 25;                // set duty cycle to 25/100 (25%)
+
+    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+
+    TACTL |= MC0;                // count up
+
+    while (1) {
+        __delay_cycles(1000000);
+        TACCR1 = 25;            // set duty cycle to 25/100 (25%)
+        __delay_cycles(1000000);
+        TACCR1 = 50;
+        __delay_cycles(1000000);
+        TACCR1 = 0;
+    }
 }

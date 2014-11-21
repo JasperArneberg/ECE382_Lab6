b1c479e5b74e37f272fe7af5636f58efc51c5c6e
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-11-20 22:20:34 -0700

    moveBack() and moveForward() subroutines

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <msp430.h>
+#define PERIOD 100;
 
 /**
  * Name: C2C Jasper Arneberg
@@ -25,23 +26,47 @@ void main(void)
     P2SEL |= BIT4;							// P2.4 is associated with TA1CCTL2
 
 	TA1CTL = ID_3 | TASSEL_2 | MC_1;		// Use 1:8 presclar off MCLK
-    TA1CCR0 = 100;							// set signal period
-
-    TA1CCR1 = 20;
-    TA1CCTL1 = OUTMOD_7;					// set TACCTL1 to Reset / Set mode
-
-    TA1CCR2 = 20;
-    TA1CCTL2 = OUTMOD_3;					// set TACCTL1 to Set / Reset mode
+    TA1CCR0 = PERIOD;							// set signal period for PWM
 
     while (1) {
-       	__delay_cycles(1000000);
-    	TA1CCR1 = 40;
-    	TA1CCR2 = 60;
-    	P2OUT ^= BIT1;						//toggle left reverse control
-    	P2OUT ^= BIT3;						//toggle right reverse control
+        //__delay_cycles(1000000);
+       	//stopMoving();
     	__delay_cycles(1000000);
-    	TA1CCR1 = 50;
-    	TA1CCR2 = 50;
+    	moveForward(20);
+    	__delay_cycles(1000000);
+    	moveForward(60);
+    	__delay_cycles(1000000);
+    	moveBack(20);
+    	__delay_cycles(1000000);
+    	moveBack(60);
+
 
     } // end loop
+}
+
+void stopMoving() {
+	TA1CCTL1 = OUTMOD_7;
+	TA1CCR1 = 0;
+	TA1CCTL2 = OUTMOD_7;
+	TA1CCR2 = 0;
+}
+
+void moveForward(char speed) {
+	P2OUT &= ~BIT1;							//clear left reverse select
+	TA1CCTL1 = OUTMOD_7;					//Reset/Set mode
+	TA1CCR1 = speed;
+
+	P2OUT &= ~BIT3;							//clear right reverse select
+	TA1CCTL2 = OUTMOD_7;					//Reset/Set mode
+	TA1CCR2 = speed;
+}
+
+void moveBack(char speed) {
+	P2OUT |= BIT1;							//set left reverse select
+	TA1CCTL1 = OUTMOD_3;					//Set/Reset mode
+	TA1CCR1 = speed;
+
+	P2OUT |= BIT3;							//set right reverse select
+	TA1CCTL2 = OUTMOD_3;					//Set/Reset mode
+	TA1CCR2 = speed;
 }

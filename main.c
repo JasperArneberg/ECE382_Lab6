f72caa25a6b9d30bdb04591a80ec3a4d4f2aef45
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-11-20 18:46:13 -0700

    both wheels driving forward

--- main.c
+++ main.c
@@ -2,34 +2,6 @@
 
 void main(void)
 {
-	/**
-    WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
-
-    P2DIR |= BIT2;                // TA0CCR1 on P2.2
-    P2SEL |= BIT2;                // TA0CCR1 on P2.2
-
-    TA1CTL &= ~MC1|MC0;            // stop timer A0
-
-    TA1CTL |= TACLR;                // clear timer A0
-
-    TA1CTL |= TASSEL1;           // configure for SMCLK
-
-    TA1CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-    TA1CCR1 = 25;                // set duty cycle to 25/100 (25%)
-
-    TA1CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-    TA1CTL |= MC0;                // count up
-
-    while (1) {
-        __delay_cycles(1000000);
-        TA1CCR1 = 25;            // set duty cycle to 25/100 (25%)
-        __delay_cycles(1000000);
-        TA1CCR1 = 50;
-        __delay_cycles(1000000);
-        TA1CCR1 = 0;
-    }
-    */
 
     WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
 
@@ -43,21 +15,24 @@ void main(void)
     P2SEL |= BIT4;							// P2.2 is associated with TA1CCTL2
 
 	TA1CTL = ID_3 | TASSEL_2 | MC_1;		// Use 1:8 presclar off MCLK
-    TA1CCR0 = 0x0100;						// set signal period
+    TA1CCR0 = 100;						// set signal period
 
-    TA1CCR1 = 0x0020;
+    TA1CCR1 = 20;
     TA1CCTL1 = OUTMOD_7;					// set TACCTL1 to Reset / Set mode
 
-    TA1CCR2 = 0x0020;
+    TA1CCR2 = 20;
     TA1CCTL2 = OUTMOD_3;					// set TACCTL1 to Reset / Set mode
 
     while (1) {
-
-    	while((P1IN & BIT3) != 0);			// Wait for a button press
-    	while((P1IN & BIT3) == 0);			// and release
-
-        TA1CCR1 = (TA1CCR1 + 0x010) & 0xFF;	// increase duty cycle
-        TA1CCR2 = (TA1CCR2 + 0x010) & 0xFF;	// decrease duty cycle
+    	__delay_cycles(1000000);
+    	TA1CCR1 = 0;
+    	TA1CCR2 = 100;
+    	__delay_cycles(1000000);
+    	TA1CCR1 = 25;            // set duty cycle to 25/100 (25%)
+    	TA1CCR2 = 75;
+    	__delay_cycles(1000000);
+    	TA1CCR1 = 50;
+    	TA1CCR2 = 50;
 
     } // end loop
 }
